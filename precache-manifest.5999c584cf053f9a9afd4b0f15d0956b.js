e6737c2adcf0e227eb6ff25d026ef09643327fa9
Ananta Gurung <anantar@live.com>
2022-07-14 16:23:00 -0400

    Updates

---
+++ precache-manifest.5999c584cf053f9a9afd4b0f15d0956b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a135939deca318613ccf",
+    "url": "/Counter-App/static/css/main.096424ee.chunk.css"
+  },
+  {
+    "revision": "a135939deca318613ccf",
+    "url": "/Counter-App/static/js/main.a135939d.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/Counter-App/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "cf970c12475f87d14158",
+    "url": "/Counter-App/static/css/2.9923d5fa.chunk.css"
+  },
+  {
+    "revision": "cf970c12475f87d14158",
+    "url": "/Counter-App/static/js/2.cf970c12.chunk.js"
+  },
+  {
+    "revision": "ccc8b98101ca97a9baf7",
+    "url": "/Counter-App/static/js/runtime~main.ccc8b981.js"
+  },
+  {
+    "revision": "2ec93c4ea6c1a98c65bfa92572a635ab",
+    "url": "/Counter-App/index.html"
+  }
+];
